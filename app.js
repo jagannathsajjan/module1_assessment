9b6129555440c915086add69750c0d3daacd1b2d
Jagannath <jagannadh.SAJJAN@gmail.com>
2020-10-07 14:28:38 +0530

    updated js file

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ angular.module('lunchCheck', [])
 .controller('LunchCheckController', LunchCheckController);
 
 LunchCheckController.$inject = ['$scope'];
-
-
-})();
+function LunchCheckController($scope){
+$scope.items="";
+console.log($scope.items.length);
+$scope.checkLunch = function(){
+    if($scope.items){
+        return $scope.items.split(',').join(' ');
+    }
+    
+if($scope.items.length>=2) {
+        $scope.message ="Enjoy";
+    }else {
+        $scope.message = "too much";
+    }
+}
+}})();
