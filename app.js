156bc3b80ccd0ee66a3294ea82c5da67be6b6d68
Jagannath <jagannadh.SAJJAN@gmail.com>
2020-10-07 14:56:12 +0530

    solution

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 (function(){
-'use strict'
+	'use strict';
 
-angular.module('lunchCheck', [])
-.controller('LunchCheckController', LunchCheckController);
+	angular.module('LunchCheck',[])
 
-LunchCheckController.$inject = ['$scope'];
-function LunchCheckController($scope){
-$scope.items="";
-console.log($scope.items.length);
-$scope.checkLunch = function(){
-    if($scope.items){
-        return $scope.items.split(',').join(' ');
+	.controller('LunchCheckController',LunchController)
+
+	LunchController.$inject=['$scope'];
+
+	function LunchController($scope){
+		$scope.items="";
+		$scope.msg="";
+		$scope.msgset = function(){
+			var NoOfItems = noItem();
+			if($scope.items===""){
+				$scope.msg="Please enter data first";
+			}
+			else if(NoOfItems > 3){
+				$scope.msg="Too Much!!";
+			}
+			else if(NoOfItems <= 3 && NoOfItems >= 1){
+				$scope.msg="Enjoy!!";
+			}
+		}
+
+function noItem(){
+    var itemarray = $scope.items.split(",");
+    var no_of_item =  itemarray.length;
+    for(var i=0;i<itemarray.length;i++){
+        if(itemarray[i]==="")
+            no_of_item--;
     }
-    
-if($scope.items.length>=2) {
-        $scope.message ="Enjoy";
-    }else {
-        $scope.message = "too much";
-    }
+    return no_of_item;
 }
-}})();
+}
+
+})();
